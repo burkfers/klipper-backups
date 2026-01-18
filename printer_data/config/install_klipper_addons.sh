ab9aee356dade6443be779d7dfb0430711106f40
gerhard <klipper-backup@gerhard>
2026-01-18 16:17:58 +0100

    New backup from 18/01/26 - 16:17:57

--- printer_data/config/install_klipper_addons.sh
+++ printer_data/config/install_klipper_addons.sh
@@ -2,3 +2,5 @@
 
 # tmc autotune
 wget -O - https://raw.githubusercontent.com/andrewmcgr/klipper_tmc_autotune/main/install.sh | bash
+# carto
+curl -s -L https://raw.githubusercontent.com/Cartographer3D/cartographer3d-plugin/refs/heads/main/scripts/install.sh | bash -s -- --klipper ~/klipper --klippy-env ~/klippy-env
