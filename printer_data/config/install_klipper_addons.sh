033cc3876308d13adcbc4f42c92ac9b4fc04850e
gerhard <klipper-backup@gerhard>
2026-05-03 14:35:01 +0200

    New backup from 03/05/26 - 14:34:58

--- printer_data/config/install_klipper_addons.sh
+++ printer_data/config/install_klipper_addons.sh
@@ -4,3 +4,7 @@
 wget -O - https://raw.githubusercontent.com/andrewmcgr/klipper_tmc_autotune/main/install.sh | bash
 # carto
 curl -s -L https://raw.githubusercontent.com/Cartographer3D/cartographer3d-plugin/refs/heads/main/scripts/install.sh | bash -s -- --klipper ~/klipper --klippy-env ~/klippy-env
+# motors_sync
+cd ~
+git clone https://github.com/MRX8024/motors-sync
+bash ~/motors-sync/install.sh
